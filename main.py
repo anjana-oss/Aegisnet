af469ad1e4d0a94bb36b0b868fab7e11e3419ddc
anjana <nnanjan8@gmail.com>
2026-06-02 21:20:09 +0530

    logging added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sqlmodel import Session
 from sqlmodel import select
 from jose import jwt
 import bcrypt
+from datetime import datetime
 
 app = FastAPI()
 
@@ -14,6 +15,14 @@ class User(SQLModel, table=True):
     username: str
     email: str
     password: str
+    role: str = "user"
+
+
+class Activitylog(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    email: str
+    action: str
+    timestamp: datetime
 
 
 class UserCreate(BaseModel):
@@ -35,6 +44,7 @@ class Update(BaseModel):
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/aegisnet"
 engine = create_engine(DATABASE_URL)
 SQLModel.metadata.create_all(engine)
+SQLModel.metadata.create_all(engine)
 SECRET_KEY = "hjgjhihu8476"
 ALGORITHM = "HS256"
 
@@ -50,7 +60,9 @@ except Exception as e:
 @app.post("/usercreate")
 def signup(newuser: UserCreate):
     with Session(engine) as session:
-        hashed_pass=bcrypt.hashpw(newuser.password.encode(),bcrypt.gensalt()).decode()
+        hashed_pass = bcrypt.hashpw(
+            newuser.password.encode(), bcrypt.gensalt()
+        ).decode()
         signup_db = User(
             username=newuser.username, email=newuser.email, password=hashed_pass
         )
@@ -61,16 +73,15 @@ def signup(newuser: UserCreate):
         return {"message": "deails added"}
 
 
-def createtoken(email):
-    payload = {"email": email}
+def createtoken(email, role):
+    payload = {"email": email, "role": role}
     token = jwt.encode(payload, SECRET_KEY, ALGORITHM)
     return token
 
+
 def verify_token(token):
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    if not payload:
-        return "token not valid"
-    return payload["email"]
+    return payload
 
 
 @app.post("/userlogin")
@@ -83,33 +94,48 @@ def login(login: LoginRequest):
         if not found_user:
             return {"message": "user not found"}
 
-        if not bcrypt.checkpw(login.password.encode(),found_user.password.encode()):
+        if not bcrypt.checkpw(login.password.encode(), found_user.password.encode()):
             return {"message": "wrong password"}
 
-        token = createtoken(found_user.email)
+        log = Activitylog(
+            email=found_user.email, action="LOGIN", timestamp=datetime.now()
+        )
+
+        session.add(log)
+        session.commit()
+
+        token = createtoken(found_user.email, found_user.role)
         return {"access_token": token}
 
 
-
 @app.get("/viewuser")
-def viewuser(token:str):
+def viewuser(token: str):
     with Session(engine) as session:
-        email=verify_token(token)
-        
-        statement = select(User).where(User.email==email)
-        found_user=session.exec(statement).first()
-        
+        payload = verify_token(token)
+        email = payload["email"]
+        statement = select(User).where(User.email == email)
+        found_user = session.exec(statement).first()
+
         if not found_user:
             return "token wrong"
-        return found_user
+        log = Activitylog(email=email, action="VIEW_PROFILE", timestamp=datetime.now())
+        session.add(log)
+        session.commit()
+
+        return {
+            "email": found_user.email,
+            "password": found_user.password,
+            "username": found_user.username,
+        }
 
 
 @app.put("/updatevalues")
-def update(new: Update,token:str):
+def update(new: Update, token: str):
     with Session(engine) as session:
-        email=verify_token(token)
-        
-        statement = select(User).where(User.email ==email)
+        payload = verify_token(token)
+        email = payload["email"]
+
+        statement = select(User).where(User.email == email)
         found_user = session.exec(statement).first()
 
         if not found_user:
@@ -118,20 +144,31 @@ def update(new: Update,token:str):
         if found_user:
             found_user.username = new.username
             found_user.password = new.password
-
             session.commit()
-            return {"message": "row updated"}
+
+        log = Activitylog(
+            email=email, action="UPDATE_PROFILE", timestamp=datetime.now()
+        )
+        session.add(log)
+        session.commit()
+        return {"message": "row updated"}
 
 
 @app.delete("/deletedata")
-def delete(token:str):
+def delete(token: str):
     with Session(engine) as session:
-        email=verify_token(token)
+        payload = verify_token(token)
+        email = payload["email"]
         statement = select(User).where(User.email == email)
         found_user = session.exec(statement).first()
 
         if not found_user:
             return {"message": "user not found"}
+        log = Activitylog(
+            email=email, action="DELETE_PROFILE", timestamp=datetime.now()
+        )
+        session.add(log)
+        session.commit()
 
         session.delete(found_user)
         session.commit()
